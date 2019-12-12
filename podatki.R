58c355104a74daba63ca44bbacd058900f820154
Gasper Pust <gapi.pust@gmail.com>
2019-12-12 09:52:13 +0100

    popravil licenco, pripravil podatke za urejanje, opisal tabele

--- podatki.R
+++ podatki.R
@@ -1,12 +1,31 @@
 library(readr)
-podatki_o_nocitvah <- read_csv('tour_occ_ninraw_1_Data.csv',col_names=c('Leto', 'Izvor_turistov', 'Enote', 'Prebivališče', 'Država', 'Vrednost', 'Zastava'),
+
+podatki_o_nocitvah <- read_csv('tour_occ_ninraw_1_Data.csv',col_names=c('leto', 'izvor_turistov', 'enote', 'prebivalisce', 'drzava', 'vrednost', 'zastava'),
                                skip=1, na=':', locale=locale(encoding='Windows-1250'))
-#podatki_o_potovanjih <- read_csv('tour_dem_ttw_1_Data.csv', col_names=c('Leto', 'Destinacija', 'Enote', 'Namen', 'Dolžina', 'Država', 'Vrednost', 'Zastava'),
-#                                 skip=1, na=':', locale=locale(encoding='Windows-1250'))
-podatki_o_nocitvah$Enote <- NULL
-podatki_o_nocitvah$Zastava <- NULL
-#podatki_o_potovanjih$Enote <- NULL
-#podatki_o_potovanjih$Zastava <- NULL
+
+podatki_o_potovanjih <- read_csv('tour_dem_ttw_1_Data.csv', col_names=c('leto', 'destinacija', 'enote', 'namen', 'dolzina', 'drzava', 'vrednost', 'zastava'),
+                                 skip=1, na=':', locale=locale(encoding='Windows-1250'))
+
+podatki_o_nocitvah$enote <- NULL
+podatki_o_nocitvah$zastava <- NULL
+podatki_o_potovanjih$enote <- NULL
+podatki_o_potovanjih$zastava <- NULL
 
 library(tidyr)
-#izvor <- gather(podatki_o_nocitvah, -leto, -enote, -prebivališče, -država, -zastava, -izvor_turistov, key=leto, value=vrednost, na.rm=TRUE)
+
+slo_nocitve <- podatki_o_nocitvah %>% filter(drzava == "Slovenia")
+slo_nocitve$drzava <- NULL
+
+hrv_nocitve <- podatki_o_nocitvah %>% filter(drzava == "Croatia")
+hrv_nocitve$drzava <- NULL
+
+slo_potovanja <- podatki_o_potovanjih %>% filter(drzava == "Slovenia")
+slo_potovanja$drzava <- NULL
+
+hrv_potovanja <- podatki_o_potovanjih %>% filter(drzava == "Croatia")
+hrv_potovanja$drzava <- NULL
+
+#vsa_slo_potovanja <- slo_potovanja %>% filter(namen == "Total")
+
+#slo_stevilo_vseh <- vsa_slo_potovanja %>% group_by(leto) %>% summarise(stevilo = sum(vrednost, na.rm = TRUE))
+#stevilo <- podatki_o_nocitvah %>% group_by(leto) %>% summarise(stevilo = sum(vrednost, na.rm = TRUE))
