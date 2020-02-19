89f36d4be402156f226bca32e9cda346632a01c3
Gasper Pust <gapi.pust@gmail.com>
2020-02-19 17:06:48 +0100

    nadaljevanje analize in risanja grafov

--- analiza/analiza.r
+++ analiza/analiza.r
@@ -17,3 +17,13 @@ graf3 <- ggplot(vse_nocitve_na_povrsino, aes(x=leto)) + geom_point(aes(y=stevilo
 graf4 <- ggplot(vsa_potovanja_na_prebivalce, aes(x=leto)) + geom_point(aes(y=stevilo_slo), color='blue') + 
   geom_line(aes(y=stevilo_slo), color='blue') + geom_point(aes(y=stevilo_hrv), color='red') + 
   geom_line(aes(y=stevilo_hrv), color='red') + ylab('stevilo potovanj na prebivalca') + ggtitle('vsa potovanja na prebivalstvo')
+
+graf5 <- ggplot(slo_nocitve, aes(x=leto)) + aes(y=vrednost, color=izvor_turistov, shape=prebivalisce) + geom_point() + geom_line() + 
+  ggtitle('sestava turistov v Sloveniji') + ylab('stevilo turistov')
+graf6 <- ggplot(hrv_nocitve, aes(x=leto)) + aes(y=vrednost, color=izvor_turistov, shape=prebivalisce) + geom_point() + geom_line() + 
+  ggtitle('sestava turistov na Hrvaskem') + ylab('stevilo turistov')
+
+graf7 <- ggplot(slo_potovanja_vse_drzave, aes(x=leto)) + aes(y=vrednost, color=dolzina, shape=namen) + geom_point() + geom_line() + 
+  ggtitle('dolzina in namen potovanja Slovencev') + ylab('stevilo turistov')
+graf8 <- ggplot(hrv_potovanja_vse_drzave, aes(x=leto)) + aes(y=vrednost, color=dolzina, shape=namen) + geom_point() + geom_line() + 
+  ggtitle('dolzina in namen potovanja Hrvatov') + ylab('stevilo turistov')
--- vizualizacija/projekt_vizualizacija.r
+++ vizualizacija/projekt_vizualizacija.r
@@ -19,34 +19,42 @@ slo_potovanja$drzava <- NULL
 hrv_potovanja <- potovanja %>% filter(drzava == "Croatia")
 hrv_potovanja$drzava <- NULL
 
+#vsa potovanja
 vsa_slo_potovanja <- slo_potovanja %>% group_by(leto) %>% summarise(stevilo = sum(vrednost, na.rm = TRUE))
 vsa_slo_potovanja$namen <- NULL
 vsa_hrv_potovanja <- hrv_potovanja %>% group_by(leto) %>% summarise(stevilo = sum(vrednost, na.rm = TRUE))
 vsa_hrv_potovanja$namen <- NULL
-
 vsa_potovanja <- vsa_slo_potovanja
 names(vsa_potovanja)[2] <- 'stevilo_slo'
 vsa_potovanja['stevilo_hrv'] <- NA
 vsa_potovanja$stevilo_hrv <- vsa_hrv_potovanja$stevilo
 
+#vsa potovanja na prebivalce
 prebivalci_slo <- 2089310
 prebivalci_hrv <- 4105493
 vsa_potovanja_na_prebivalce <- vsa_potovanja
 vsa_potovanja_na_prebivalce$stevilo_slo <- (vsa_potovanja$stevilo_slo) / prebivalci_slo
 vsa_potovanja_na_prebivalce$stevilo_hrv <- (vsa_potovanja$stevilo_hrv) / prebivalci_hrv
 
+#vse nočitve
 vse_slo_nocitve <- slo_nocitve %>% group_by(leto) %>% summarise(stevilo = sum(vrednost, na.rm = TRUE))
 vse_slo_nocitve$prebivalisce <- NULL
 vse_hrv_nocitve <- hrv_nocitve %>% group_by(leto) %>% summarise(stevilo = sum(vrednost, na.rm = TRUE))
 vse_hrv_nocitve$prebivalisce <- NULL
-
 vse_nocitve <- vse_slo_nocitve
 names(vse_nocitve)[2] <- 'stevilo_slo'
 vse_nocitve['stevilo_hrv'] <- NA
 vse_nocitve$stevilo_hrv <- vse_hrv_nocitve$stevilo
 
+#vse nočitve na površino
 povrsina_slo <- 20273
 povrsina_hrv <- 56542
 vse_nocitve_na_povrsino <- vse_nocitve
 vse_nocitve_na_povrsino$stevilo_slo <- (vse_nocitve$stevilo_slo) / povrsina_slo
 vse_nocitve_na_povrsino$stevilo_hrv <- (vse_nocitve$stevilo_hrv) / povrsina_hrv
+
+#potovanja v vse države skupaj
+slo_potovanja_vse_drzave <- slo_potovanja %>% filter(destinacija == 'All countries of the world')
+slo_potovanja_vse_drzave$destinacija <- NULL
+hrv_potovanja_vse_drzave <- hrv_potovanja %>% filter(destinacija == 'All countries of the world')
+hrv_potovanja_vse_drzave$destinacija <- NULL
