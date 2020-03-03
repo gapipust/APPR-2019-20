ec4c235a4381b35ee42297a2ce8d5ed3c4db0bd8
Gasper Pust <gapi.pust@gmail.com>
2020-03-03 14:21:29 +0100

    popravki na grafih napovedi

--- analiza/analiza.r
+++ analiza/analiza.r
@@ -52,31 +52,38 @@ lin <- lm(data=vse_slo_nocitve_pr, stevilo ~ leto)
 pr <- predict(lin, data.frame(leto=seq.int(2018, 2025, 1)))
 vse_slo_nocitve1 <- vse_slo_nocitve_pr
 vse_slo_nocitve1 <- data.frame(leto = c(2018, 2019, 2020, 2021, 2022, 2023, 2024, 2025), stevilo = c(pr[1], pr[2], pr[3], pr[4], pr[5], pr[6], pr[7], pr[8]))
+vse_slo_nocitve_pr$vrsta <- "podatki"
+vse_slo_nocitve1$vrsta <- "napoved"
 vse_slo_nocitve_pr <- rbind(vse_slo_nocitve_pr, vse_slo_nocitve1)
+
 
 vse_hrv_nocitve_pr <- vse_hrv_nocitve
 lin1 <- lm(data=vse_hrv_nocitve_pr, stevilo ~ leto)
 pr1 <- predict(lin1, data.frame(leto=seq.int(2019, 2025, 1)))
 vse_hrv_nocitve1 <- vse_hrv_nocitve_pr
 vse_hrv_nocitve1 <- data.frame(leto = c(2019, 2020, 2021, 2022, 2023, 2024, 2025), stevilo = c(pr1[1], pr1[2], pr1[3], pr1[4], pr1[5], pr1[6], pr1[7]))
+vse_hrv_nocitve_pr$vrsta <- "podatki"
+vse_hrv_nocitve1$vrsta <- "napoved"
 vse_hrv_nocitve_pr <- rbind(vse_hrv_nocitve_pr, vse_hrv_nocitve1)
 
 vse_nocitve_pr <- vse_slo_nocitve_pr
 names(vse_nocitve_pr)[2] <- 'stevilo_slo'
 vse_nocitve_pr['stevilo_hrv'] <- NA
 vse_nocitve_pr$stevilo_hrv <- vse_hrv_nocitve_pr$stevilo
-vse_nocitve_pr <- vse_nocitve_pr %>% filter(leto != 2010, leto != 2011, leto != 2012, leto != 2013, leto != 2014, leto != 2015)
 
-graf9 <- ggplot(vse_nocitve_pr, aes(x=leto)) + geom_point(aes(y=stevilo_slo, colour="Slovenija")) + geom_line(aes(y=stevilo_slo, colour="Slovenija")) + 
-  geom_point(aes(y=stevilo_hrv, colour="Hrvaška")) + geom_line(aes(y=stevilo_hrv, colour="Hrvaška")) + 
+graf9 <- ggplot(vse_nocitve_pr, aes(x=leto)) + geom_point(aes(y=stevilo_slo, colour="Slovenija", shape=vrsta)) + geom_line(aes(y=stevilo_slo, colour="Slovenija")) + 
+  geom_point(aes(y=stevilo_hrv, colour="Hrvaška", shape=vrsta)) + geom_line(aes(y=stevilo_hrv, colour="Hrvaška")) + 
   scale_colour_manual("", values = c("Slovenija" = "blue", "Hrvaška" = "red")) + ylab('predvideno število nocitev v prihodnosti') + 
-  ggtitle('Napoved vseh nočitev za prihodnost') + scale_x_continuous(breaks = 1*2016:2025) + scale_y_continuous(labels=comma_format(big.mark=""))
+  ggtitle('Napoved vseh nočitev za prihodnost') + scale_x_continuous(breaks = 1*2010:2025) + scale_y_continuous(labels=comma_format(big.mark="")) +
+  geom_smooth(method="lm", aes(y=stevilo_slo, colour="Slovenija")) + geom_smooth(method="lm", aes(y=stevilo_hrv, colour="Hrvaška"))
 
 vsa_slo_potovanja_pr <- vsa_slo_potovanja
 lin2 <- lm(data=vsa_slo_potovanja_pr, stevilo ~ leto)
 pr2 <- predict(lin2, data.frame(leto=seq.int(2019, 2025, 1)))
 vsa_slo_potovanja1 <- vsa_slo_potovanja_pr
 vsa_slo_potovanja1 <- data.frame(leto = c(2019, 2020, 2021, 2022, 2023, 2024, 2025), stevilo = c(pr2[1], pr2[2], pr2[3], pr2[4], pr2[5], pr2[6], pr2[7]))
+vsa_slo_potovanja_pr$vrsta <- "podatki"
+vsa_slo_potovanja1$vrsta <- "napoved"
 vsa_slo_potovanja_pr <- rbind(vsa_slo_potovanja_pr, vsa_slo_potovanja1)
 
 vsa_hrv_potovanja_pr <- vsa_hrv_potovanja
@@ -84,15 +91,17 @@ lin3 <- lm(data=vsa_hrv_potovanja_pr, stevilo ~ leto)
 pr3 <- predict(lin3, data.frame(leto=seq.int(2019, 2025, 1)))
 vsa_hrv_potovanja1 <- vsa_hrv_potovanja_pr
 vsa_hrv_potovanja1 <- data.frame(leto = c(2019, 2020, 2021, 2022, 2023, 2024, 2025), stevilo = c(pr3[1], pr3[2], pr3[3], pr3[4], pr3[5], pr3[6], pr3[7]))
+vsa_hrv_potovanja_pr$vrsta <- "podatki"
+vsa_hrv_potovanja1$vrsta <- "napoved"
 vsa_hrv_potovanja_pr <- rbind(vsa_hrv_potovanja_pr, vsa_hrv_potovanja1)
 
 vsa_potovanja_pr <- vsa_slo_potovanja_pr
 names(vsa_potovanja_pr)[2] <- 'stevilo_slo'
 vsa_potovanja_pr['stevilo_hrv'] <- NA
 vsa_potovanja_pr$stevilo_hrv <- vsa_hrv_potovanja_pr$stevilo
-vsa_potovanja_pr <- vsa_potovanja_pr %>% filter(leto != 2010, leto != 2011, leto != 2012, leto != 2013, leto != 2014, leto != 2015)
 
-graf10 <- ggplot(vsa_potovanja_pr, aes(x=leto)) + geom_point(aes(y=stevilo_slo, colour="Slovenija")) +  geom_line(aes(y=stevilo_slo, colour="Slovenija")) + 
-  geom_point(aes(y=stevilo_hrv, colour="Hrvaška")) + geom_line(aes(y=stevilo_hrv, colour="Hrvaška")) + 
+graf10 <- ggplot(vsa_potovanja_pr, aes(x=leto)) + geom_point(aes(y=stevilo_slo, colour="Slovenija", shape=vrsta)) +  geom_line(aes(y=stevilo_slo, colour="Slovenija")) + 
+  geom_point(aes(y=stevilo_hrv, colour="Hrvaška", shape=vrsta)) + geom_line(aes(y=stevilo_hrv, colour="Hrvaška")) + 
   scale_colour_manual("", values = c("Slovenija" = "blue", "Hrvaška" = "red")) + ylab('predvideno število potovanj v prihodnosti') + 
-  ggtitle('Napoved vseh potovanj za prihodnost') + scale_x_continuous(breaks = 1*2016:2025) + scale_y_continuous(labels=comma_format(big.mark=""))
+  ggtitle('Napoved vseh potovanj za prihodnost') + scale_x_continuous(breaks = 1*2010:2025) + scale_y_continuous(labels=comma_format(big.mark="")) +
+  geom_smooth(method="lm", aes(y=stevilo_slo, colour="Slovenija")) + geom_smooth(method="lm", aes(y=stevilo_hrv, colour="Hrvaška"))
